c66644b341a93a5348c26b3e872b419cc5d94a05
Mayank Aggarwal <56104618+mayank623@users.noreply.github.com>
2023-10-18 21:24:25 +0530

    Time: 20 ms (8.52%), Space: 20.7 MB (25.15%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -2,62 +2,90 @@ class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
 
-        // int size= flowerbed.size();
-        // int i=0;
+        int size= flowerbed.size();
+        int i=0;
 
-        // if(n>size)return false;
+        if(n>size)return false;
 
-        // if(size==1) return ((n==1 and flowerbed[0]==0)||(n==0));
+        if(size==1) return ((n==1 and flowerbed[0]==0)||(n==0));
         
-        // while(i<size)
-        // {
-        //     if(i==0)
-        //     {
-        //         if(flowerbed[i]==0 and flowerbed[i+1]==0)
-        //         {
-        //             flowerbed[i]=1;
-        //             n--;
+        while(i<size)
+        {
+            if(flowerbed[i]==0)
+            {
+                if(i==0)
+                {
+                    if(flowerbed[i+1]==0)
+                    {
+                        flowerbed[i]=1;
+                        n--;
+                    }
+                }
+                else if(i==size-1)
+                {
+                    if(flowerbed[i-1]==0)
+                    {
+                        flowerbed[i]=1;
+                        n--;
+                    }
+                }
+                else
+                {
+                    if(flowerbed[i+1]==0 and flowerbed[i-1]==0)
+                    {
+                        flowerbed[i]=1;
+                        n--;
+                    }
+                }
+
+            }
+            // if(i==0)
+            // {
+            //     if(flowerbed[i]==0 and flowerbed[i+1]==0)
+            //     {
+            //         flowerbed[i]=1;
+            //         n--;
+            //     }
+            // }
+
+            // else if(i==size-1)
+            // {
+            //     if(flowerbed[i]==0 and flowerbed[i-1]==0)
+            //     {
+            //         flowerbed[i]=1;
+            //         n--;
+            //     }   
+            // }
+
+            // else
+            // {
+            //     if(flowerbed[i]==0 and flowerbed[i+1]==0 and flowerbed[i-1]==0)
+            //     {
+            //         flowerbed[i]=1;
+            //         n--;
+            //     }   
+            // }
+
+            i++;
+        }
+
+        if(n<=0)return true;
+
+
+        return false;
+
+        // if (n == 0) {
+        //     return true;
+        // }
+        // for (int i = 0; i < flowerbed.size(); i++) {
+        //     if (flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == flowerbed.size()-1 || flowerbed[i+1] == 0)) {
+        //         flowerbed[i] = 1;
+        //         n--;
+        //         if (n == 0) {
+        //             return true;
         //         }
         //     }
-
-        //     else if(i==size-1)
-        //     {
-        //         if(flowerbed[i]==0 and flowerbed[i-1]==0)
-        //         {
-        //             flowerbed[i]=1;
-        //             n--;
-        //         }   
-        //     }
-
-        //     else
-        //     {
-        //         if(flowerbed[i]==0 and flowerbed[i+1]==0 and flowerbed[i-1]==0)
-        //         {
-        //             flowerbed[i]=1;
-        //             n--;
-        //         }   
-        //     }
-
-        //     i++;
         // }
-
-        // if(n<=0)return true;
-
-
         // return false;
-
-        if (n == 0) {
-            return true;
-        }
-        for (int i = 0; i < flowerbed.size(); i++) {
-            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == flowerbed.size()-1 || flowerbed[i+1] == 0)) {
-                flowerbed[i] = 1;
-                n--;
-                if (n == 0) {
-                    return true;
-                }
-            }
-        }
-        return false;
     }
 };

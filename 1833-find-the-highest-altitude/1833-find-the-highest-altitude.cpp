a62cb6f2e23975e740e1301d3ee799e8338bcee0
Mayank Aggarwal <56104618+mayank623@users.noreply.github.com>
2023-11-07 01:06:23 +0530

    Time: 0 ms (100%), Space: 8.6 MB (7.58%) - LeetHub

---
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+
+        int maxx=0;
+        int curr=0;
+
+        for(int i=0; i<gain.size(); i++)
+        {
+            curr+=gain[i];
+
+            maxx=max(maxx,curr);
+        }
+
+        return maxx;
+        
+    }
+};

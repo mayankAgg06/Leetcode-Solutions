b2eeee779d3af07766c134b29e7b2b82f4e84b81
Mayank Aggarwal <56104618+mayankAgg06@users.noreply.github.com>
2024-07-21 19:00:28 +0530

    Time: 2 ms (46%), Space: 12 MB (6.6%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -16,11 +16,12 @@ public:
         
         if (!root) return NULL;
         
-        TreeNode* left = invertTree(root->left);
-        TreeNode* right = invertTree(root->right);
-        
-        root->left = right;
-        root->right = left;
+        TreeNode* temp = root->left;
+        root->left = root->right ;
+        root->right = temp;
+
+        root->left = invertTree(root->left);
+        root->right = invertTree(root->right);
         
         return root;
     }

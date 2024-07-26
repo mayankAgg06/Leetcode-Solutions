8db7043e5237249e86660ce1a1e0783ae0a0cb61
Mayank Aggarwal <56104618+mayankAgg06@users.noreply.github.com>
2024-07-26 23:14:52 +0530

    Time: 7 ms (61.14%), Space: 12.9 MB (14.73%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -19,8 +19,22 @@ public:
         int n=grid.size()-1;
         int m=grid[0].size()-1;
 
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
 
-        return pathSum(grid,n,m,dp);
+        dp[0][0]= grid[0][0];
+
+        for(int i=1; i<=n; i++) dp[i][0] = dp[i-1][0] + grid[i][0];
+
+        for(int j=1; j<=m; j++) dp[0][j] = dp[0][j-1] + grid[0][j];
+
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=m; j++)
+            {
+                dp[i][j] = grid[i][j] + min(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+
+        return dp[n][m];
     }
 };

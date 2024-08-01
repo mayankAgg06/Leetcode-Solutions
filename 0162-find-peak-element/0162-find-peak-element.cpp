5b1f83a27a81dcccd7ba50695994f6980f7a7775
Mayank Aggarwal <56104618+mayankAgg06@users.noreply.github.com>
2024-08-01 15:44:30 +0530

    Time: 0 ms (100%), Space: 11.4 MB (27.8%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -4,13 +4,17 @@ public:
 
         int e = nums.size()-1;
         int s = 0;
-    
+        
         while(s<e)
         {
             int m = s + (e-s)/2;
-            if(nums[m] > nums[m+1]) e = m;
-            else s = m+1;
+
+            if(nums[m]>nums[m+1]) e=m;
+
+            else
+            s=m+1;
         }
+
         return e;
     }
 };

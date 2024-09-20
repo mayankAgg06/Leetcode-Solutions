fe661e708078fe8c724adfb704203ecb00d50409
Mayank Aggarwal <56104618+mayankAgg06@users.noreply.github.com>
2024-09-20 10:33:27 +0530

    Time: 48 ms (38.56%), Space: 51.1 MB (42.42%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,40 +1,11 @@
 class Solution {
 public:
-    bool jumping(int ind, vector<int > &nums, vector<int> &dp)
-    {
-        if(ind>=nums.size()-1)return true;
-        
-        if(dp[ind]!=-1) return dp[ind];
-
-        // cout<<"on index "<<ind;
-
-        if((nums.size()-ind-1)<nums[ind]) return dp[ind]=1;
-
-        for(int j=1; j<=nums[ind]; j++)
-        {
-            // cout<<"testing index "<<ind+j;
-            if(jumping(ind+j,nums,dp)) return dp[ind]=1;
-        }
-
-        return dp[ind]=0;
-    }
     bool canJump(vector<int>& nums) {
-        
-        int n=nums.size();
-
-        vector<int> dp(n,-1);
-
-        if(n==1)return true;
-
-        dp[n-1]=true;
-
-        for(int j=1; j<=nums[0]; j++)
-        {
-            // cout<<"testing number"<<j;
-            if(jumping(j,nums,dp)) return true;
+        int maxInd=0;
+        for(int i=0;i<nums.size();i++){
+            if(i>maxInd) return false;
+            maxInd= max(maxInd,i+nums[i]);
         }
-
-        return false;
-       
+        return true;
     }
 };

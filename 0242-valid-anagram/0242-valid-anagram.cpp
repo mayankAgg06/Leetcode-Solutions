a14057c322822061afaeb9eda0353309c8a61614
Mayank Aggarwal <56104618+mayankAgg06@users.noreply.github.com>
2025-07-31 12:55:48 +0530

    Time: 0 ms (100%), Space: 9.6 MB (74.12%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char,int> freq;
+        // unordered_map<char,int> freq;
 
-        for(auto i: s) freq[i]++;
+        vector<int> freq(26,0);
 
-        for(auto i:t) freq[i]--;
+        for(auto i: s) freq[i-'a']++;
+
+        for(auto i:t) freq[i-'a']--;
 
         for(auto i:freq){
-            if(i.second != 0) return false;
+            if(i!= 0) return false;
         } 
 
         return true;
